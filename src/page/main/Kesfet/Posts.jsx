d5e521ddd1328b78c490efbd23bb19a62b6cb37a
Fatih Şen <fatihydrm@outlook.com>
2022-09-11 23:10:31 +0300

    update Kesfet page

---
+++ src/page/main/Kesfet/Posts.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import Datas from "./datas";
+
+function Posts() {
+  return (
+    <>
+      <ul className='flex flex-col gap-4 w-full'>
+        {Datas &&
+          Datas.map((data, index) => (
+            <li
+              className='border p-3 dark:border-darkBorder rounded-sm'
+              key={index}
+            >
+              <div>
+                <div className='gap-2 flex flex-col 2xl:flex-row xl:flex-row lg:flex-row md:flex-col sm:flex-col'>
+                  <a className='inline-flex items-center gap-2 ' href='#'>
+                    <img
+                      className='w-5 object-cover'
+                      src={data.avatar}
+                      alt={data.avatar}
+                    />
+                    <span className='text-mainGray dark:text-mainBlue'>
+                      {data.user}
+                    </span>
+                  </a>
+                  <p className='dark:text-white'>
+                    tarafından{" "}
+                    <span className='opacity-70 dark:opacity-20'>
+                      {data.time}
+                    </span>{" "}
+                    paylaşıldı.
+                  </p>
+                </div>
+                <q className='italic my-2 dark:text-white/80'>
+                  <span className='mx-1.5'>{data.comment}</span>
+                </q>
+              </div>
+              <a
+                className='flex flex-col 2xl:flex-row xl:flex-row lg:flex-row md:flex-col sm:flex-col bg-lightGreen p-3 dark:bg-darkBlueThree rounded-sm items-center gap-3'
+                href={data.url}
+                target='_blank'
+              >
+                {data.img !== "" && (
+                  <img
+                    className='w-full 2xl:w-24 xl:w-24 lg:w-24 md:w-full sm:w-full object-cover'
+                    src={data.img}
+                    alt={data.img}
+                  />
+                )}
+                <div>
+                  <span className='text-mainGray font-semibold dark:text-mainBlue'>
+                    {data.title}
+                  </span>
+                  <p className='text-darkTwo/90 dark:text-white/50'>
+                    {data.desc}
+                  </p>
+                </div>
+              </a>
+            </li>
+          ))}
+      </ul>
+    </>
+  );
+}
+
+export default Posts;
---
+++ src/page/main/Kesfet/datas.jsx
@@ -0,0 +1,73 @@
+const Datas = [
+  {
+    user: "fatihsen",
+    avatar:
+      "https://www.gravatar.com/avatar/6ca72f35b9a6e4ceb37f4ce2e5569947?s=80&d=mp&r=g",
+    time: "32 gün önce",
+    comment: "Bayrak iconları için kullanabileceğiniz kullanışlı bir cdn",
+    title: "CDN & API of flags – Flagcdn.com",
+    img: "",
+    url: "https://flagcdn.com/",
+    desc: "Free service providing an easy solution to embed flags on your website or for programmatically download to keep flags in your projects up-to-date.",
+  },
+  {
+    user: "tayfunerbilen",
+    avatar:
+      "https://www.gravatar.com/avatar/8b0987af0a615d1535491eca8c1362e4?s=80&d=mp&r=g",
+    time: "7 gün önce",
+    comment:
+      "Olası güvenlik açıklarını senaryolaştırarak ve örneklerle anlatan harika bir site",
+    title: "Learn to Hack",
+    img: "https://d1o5lmfp4gliop.cloudfront.net/assets/screenshots/command-execution-3-f1e144ffdccee1215bbe432d8e1250fcfcaf825b4de2bce56c291e8ba9662e76.png",
+    url: "https://www.hacksplaining.com/",
+    desc: "Hacksplaining: Security Training for Developers",
+  },
+  {
+    user: "bahlulhasanli",
+    avatar:
+      "https://www.gravatar.com/avatar/c2759a016af73b48dfe7c37353cb87e0?s=80&d=mp&r=g",
+    time: "52 gün önce",
+    comment:
+      "css ile scroll olabilen alanları gölgelendirmek için güzel bir generator",
+    title: "CSS scroll shadows",
+    img: "",
+    url: "https://css-scroll-shadows.vercel.app/",
+    desc: "https://css-scroll-shadows.vercel.app/",
+  },
+  {
+    user: "ugurozgen",
+    avatar:
+      "https://www.gravatar.com/avatar/4bfe858f656ca9cdd9ac7cd26076272a?s=80&d=mp&r=g",
+    time: "14 gün önce",
+    comment: "headlessui alternativi, stilsiz accessible ui componentler",
+    title: "Primitives — Radix UI",
+    img: "https://radix-ui.com/social/default.png",
+    url: "https://www.radix-ui.com/",
+    desc: "An open-source React component library for building high-quality, accessible design systems and web apps.",
+  },
+  {
+    user: "tayfunpolat",
+    avatar:
+      "https://www.gravatar.com/avatar/02308fb40e1c5834361bdbd8948886ac?s=80&d=mp&r=g",
+    time: "2gün önce",
+    comment:
+      "react'te accessible ui componentler yazmanız için harika bir paket",
+    title: "PHP Versions • PHP.Watch",
+    img: "",
+    url: "https://php.watch/versions",
+    desc: "https://php.watch/versions",
+  },
+  {
+    user: "ozzwizard",
+    avatar:
+      "https://www.gravatar.com/avatar/2733c74d70d1eb9c9f1c77f531363076?s=80&d=mp&r=g",
+    time: "34 gün önce",
+    comment: "react için talwindcss kurulumu",
+    title: "Headless UI – Unstyled, fully accessible UI components",
+    img: "https://headlessui.dev/_next/static/media/social-card.3e0b1ed1aac3c1db62a0a1e7023d250b.jpg",
+    url: "https://headlessui.dev/",
+    desc: "https://headlessui.dev/",
+  },
+];
+
+export default Datas;

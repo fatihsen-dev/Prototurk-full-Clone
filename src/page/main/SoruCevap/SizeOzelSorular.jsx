8bbedc594bc869ce93d665796d8896589e20d432
Fatih Şen <fatihydrm@outlook.com>
2022-09-17 02:29:07 +0300

    fix

--- src/page/main/SoruCevap/SizeOzelSorular.jsx
+++ src/page/main/SoruCevap/SizeOzelSorular.jsx
@@ -1,4 +1,15 @@
+import React, {useEffect, useContext} from "react";
+import mainContext from "../../../context/mainContext";
+
 function SizeOzelSorular() {
+  const {setActiveSoruCevapSidebar} = useContext(mainContext);
+
+  useEffect(() => {
+    setActiveSoruCevapSidebar(true);
+    document.title = "Takip Ettiğim Sorular - PROTOTURK";
+    
+    return () => setActiveSoruCevapSidebar(false);
+  }, []);
   return (
     <div className='overflow-auto dark:bg-dark h-full w-full flex-1 flex flex-col items-start pb-10 px-5 2xl:px-40 xl:px-24 lg:px-14 md:px-5 sm:px-5'>
       <div className='flex-1 flex flex-col items-start container mx-auto py-8'>
